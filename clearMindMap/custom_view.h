e4d74eb0b7fbef6595f2d0d576c0e10db811df82
KamilDebek <105932051+KamilDebek@users.noreply.github.com>
2023-02-16 08:57:24 +0100

    Add files via upload

---
+++ clearMindMap/custom_view.h
@@ -0,0 +1,28 @@
+#ifndef CUSTOM_VIEW_H
+#define CUSTOM_VIEW_H
+
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <QWheelEvent>
+#include <QKeyEvent>
+#include <mainwindow.h>
+class Custom_View: public QGraphicsView
+{
+public:
+    explicit Custom_View(QWidget *parent = nullptr);
+    qreal panSpeed;
+    qreal zoomDelta;
+    void pan(QPointF delta);
+    void zoom(float scaleFactor);
+    void zoomIn();
+    void zoomOut();
+protected:
+    virtual void wheelEvent(QWheelEvent *event) override;
+    using QGraphicsView::QGraphicsView;
+private:
+    qreal _scale;
+    bool _doMousePanning;
+    bool _doKeyZoom;
+};
+
+#endif // CUSTOM_VIEW_H
